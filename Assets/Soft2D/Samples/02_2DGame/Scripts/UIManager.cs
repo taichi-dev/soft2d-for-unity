eaa84301244b2c4689af9b5c9a024a30c65339a9
karmotrine <657493712@qq.com>
2023-06-28 16:26:11 +0800

    Change Files' Path

--- Assets/Soft2D/Samples/02_2DGame/Scripts/UIManager.cs
+++ Assets/Soft2D/Samples/02_2DGame/Scripts/UIManager.cs
@@ -13,8 +13,7 @@ public class UIManager : MonoBehaviour
     public Button skipButton;
     public Button nextStageButton;
     public GameObject settingPanel;
-    public GameObject warningText;
-    
+
     [Tooltip("UI Slot Group")] public List<GameObject> slotGroup;
     
     public SkinData currData;
@@ -22,7 +21,6 @@ public class UIManager : MonoBehaviour
     private void Start()
     {
         string sceneName = SceneManager.GetActiveScene().name;
-        warningText.SetActive(false);
 
         settingsButton?.onClick.AddListener(PauseLevel);
         backButton?.onClick.AddListener(PauseLevel);
--- Assets/Soft2D/Samples/Scripts/RendererDataWarning.cs
+++ Assets/Soft2D/Samples/Scripts/RendererDataWarning.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class WarningLog : MonoBehaviour
+public class RendererDataWarning : MonoBehaviour
 {
     public GameObject warningText;
 
