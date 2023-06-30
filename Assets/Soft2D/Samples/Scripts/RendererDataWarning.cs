52bb9666ea97ec193196fda4068ec658ab853253
yucrazing <723284893@qq.com>
2023-06-30 17:12:52 +0800

    Update docs

--- Assets/Soft2D/Samples/Scripts/RendererDataWarning.cs
+++ Assets/Soft2D/Samples/Scripts/RendererDataWarning.cs
@@ -6,7 +6,7 @@ public class RendererDataWarning : MonoBehaviour
 {
     public string warning = "Please configure the renderer data correctly before running this scene:\n\n" +
                             "1. Find the file location of 'Render Pipeline Asset' currently used by this project: Project Settings ->Quality -> Rendering -> Render Pipeline Asset.\n\n" + 
-                            "2. Click that 'Render Pipline Asset' asset file and click \"+\" to add a renderer data into its  'Renderer List' in its Inspector window, select 'Kawase Blur Data' as the new renderer data.\n\n" +
+                            "2. Click that 'Render Pipline Asset' asset file and click '+' to add a renderer data into its  'Renderer List' in its Inspector window, select 'Kawase Blur Data' as the new renderer data.\n\n" +
                             "(Disable the 'WarningCanvas' object in the scene to close this message.)";
     public Text warningText;
 
